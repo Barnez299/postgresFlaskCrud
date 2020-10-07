4829c03b09380465b6aadb20a33b0b24202759dd
barnez299 <barnez29@gmail.com>
2020-10-07 18:49:03 +0200

    add student modal working

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import psycopg2
 app = Flask(__name__)
 
 
+# View all data on Index Page
+
 @app.route('/')
 def Index():
 
@@ -18,15 +20,82 @@ def Index():
         cur.execute("SELECT * FROM students")
         data = cur.fetchall()
         cur.close()
+
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
 
-
     return render_template('index.html', data=data)
 
 
-if__name__ = '__main__'
-app.run(debug=True)
+# Insert/Add employee route
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+    
+    conn = None
+    stud_id = None
+
+    try:
+
+        name = request.form['name']
+        email = request.form['email']
+
+        if name and email and request.method == 'POST':
+            params = config()
+            conn = psycopg2.connect(**params)
+            cur = conn.cursor()
+
+            sql = "INSERT INTO students(stud_name, stud_email) VALUES  (%s, %s) RETURNING stud_id";
+            data = (name, email)
+            cur.execute(sql, data)
+            stud_id = cur.fetchone()[0]
+            conn.commit()
+            cur.close()
+
+            return redirect(url_for('Index'))
+
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print(error)
+    finally:
+
+        if conn is not None:
+            conn.close()
+
+    return stud_id
+
+        
+
+        
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
